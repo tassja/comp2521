3ad367e8b4a9eed8852bca8a99a8b99de9a0f4bd
Tassja Kriek <xtassja@gmail.com>
2017-09-30 02:54:11 +1000

    tryingto open text file according to array, doesnt compile

--- pagerank.c
+++ pagerank.c
@@ -9,15 +9,24 @@ int main(int argc, char const *argv[]) {
 
      FILE *fp = fopen("Sample1/collection.txt", "r");
 
-
      int array[99];
      int i = 0;
 
+        fscanf(fp,"\n");
         while (fscanf(fp,"url%d " , &array[i]) != EOF) {
+
             printf("%d\n", array[i]);
             i++;
         }
         fclose(fp);
+        int size = i;
+
+        while (size >= 0) {
+            fp = fopen("Sample1/url"array[size]".txt", "r");
+            fscanf(fp,"#start Section-1");
+            fscanf(fp,"\n");
+            size--;
+        }
 
     return 0;
 }
