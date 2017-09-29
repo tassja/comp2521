b839ee2b1b929eb4fad6f22fa710f9ef54c60ddc
Tassja Kriek <xtassja@gmail.com>
2017-09-30 02:34:46 +1000

    input file names into array for matrix or list rep

--- pagerank.c
+++ pagerank.c
@@ -1,1 +1,23 @@
-.
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "graph.h"
+
+
+int main(int argc, char const *argv[]) {
+
+     FILE *fp = fopen("Sample1/collection.txt", "r");
+
+
+     int array[99];
+     int i = 0;
+
+        while (fscanf(fp,"url%d " , &array[i]) != EOF) {
+            printf("%d\n", array[i]);
+            i++;
+        }
+        fclose(fp);
+
+    return 0;
+}
