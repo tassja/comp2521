5ed2132ecb2d07aeacdba9580d1967204f0c7406
tassja <xtassja@gmail.com>
2017-10-21 22:19:10 +1100

    final pagerank.c with comments

--- pagerank.c
+++ pagerank.c
@@ -7,7 +7,7 @@
 // Calculate Weighted PageRanks                                       //
 // graph.c ADT courtesy of John Shepherd, September 2015              //
 ////////////////////////////////////////////////////////////////////////
-#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -48,8 +48,7 @@ double win(int, int, RankCollection,Graph);
 double wout(int, int, RankCollection,Graph);
 
 int main(int argc, char **argv) {
-    int i = 0; //iterator
-
+    int i = 0;
     if(argc != 4) {
         fprintf(stderr, "Usage: %s d diffPR maxIterations\n", argv[0]);
         return 1; //do not pass go do not collect $200
@@ -60,14 +59,13 @@ int main(int argc, char **argv) {
     float diffPR = atof(argv[2]);
     float maxIteration = atof(argv[3]);
 
-    //get set from readdata.c and graph, seperate functions??
-    Graph urlgraph = newGraph(999);
+    Graph urlgraph = newGraph(999); //make graph to represent the url connections
     urlgraph = creategraph();
     RankCollection urldata;
-    urldata.nurls = nVertices(urlgraph);
-    urldata.data = malloc(sizeof(Ranks)*urldata.nurls);
-    while (i < urldata.nurls) { //populate url collection with the entered urls
-        //urldata.data[i].url = findurlstring(urldata, i);
+    urldata.nurls = nVertices(urlgraph); //make the urldata structure big enough to load all urls
+    urldata.data = malloc(sizeof(Ranks)*urldata.nurls); //get some
+
+    while (i < urldata.nurls) { //initlise variables inurldata
         urldata.data[i].pRank = 0;
         urldata.data[i].prevpRank = 0;
         urldata.data[i].outlinks = 0;
@@ -78,41 +76,28 @@ int main(int argc, char **argv) {
     //calculate outlinks for each url
     for (i = 0; i < nVertices(urlgraph); i++) {
         for (j = 0; j < nVertices(urlgraph); j++) {
-            if (urlgraph->edges[i][j] == 1) {
+            if (urlgraph->edges[i][j] == 1) { //url has an outgoing link here
                 urldata.data[i].outlinks++;
             }
         }
-        //printf("outlinks:%lf\n", urldata.data[i].outlinks);
     }
 
-    //calculate inlinks for each url
+    //calculate inlinks for each url and store related inlinks in array
     int N = nVertices(urlgraph);
-    //printf("N is: %d\n", N);
     int inlinks[N][N];
     for (j = 0; j < N; j++) {
-        //int b = 0;
         for (i = 0; i < N; i++) {
-            if (urlgraph->edges[i][j] == 1) {
+            if (urlgraph->edges[i][j] == 1) { //url has ingoing link here
                 urldata.data[j].inlinks++;
                 inlinks[j][i] = 1;
-                //printf("j: [%d] i: [%d] b: [%d]\n",j,i,b);
-
-                //b++;
             }else{
                 inlinks[j][i] = 0;
-                //b++;
             }
-            //printf("inlinks[%d][%d]:%d\n", j,i,inlinks[j][i]);
         }
-        //printf("inlinks:%lf\n", urldata.data[j].inlinks);
     }
 
-    calculatePageRank(urlgraph, urldata, d, diffPR, maxIteration,N,inlinks);
-    //to do: writeRanksFile(seturls);
-    exportPageRank(urlgraph,urldata,N);
-    for (int c = 0; c < nVertices(urlgraph); c++) {
-        //printf("url, %.0f, %.7f\n", urldata.data[c].outlinks,urldata.data[c].pRank);
-    }
+    calculatePageRank(urlgraph, urldata, d, diffPR, maxIteration,N,inlinks); //calculate page rank according to formula given
+    exportPageRank(urlgraph,urldata,N); //export sorted pageranks to txt file
     return 0;
 }
 
@@ -120,44 +105,28 @@ void calculatePageRank(Graph urlgraph, RankCollection urldata, float d, float di
     N = nVertices(urlgraph);
     int c,k,w;
     for (c = 0; c < N; c++) {
-        urldata.data[c].pRank = 1/(float)N;
+        urldata.data[c].pRank = 1/(float)N; //set inital page ranks
     }
 
     float prePR;
     int iteration = 0;
     float sum = 0;
-    float diff = diffPR;
+    float diff = diffPR; //enter iteration
 
     while ((iteration < maxIteration) && diff >= (diffPR)) {
             diff = 0;
             for (k = 0; k < N; k++) {
-                prePR = urldata.data[k].pRank;
+                prePR = urldata.data[k].pRank; //store previous pagerank to calculate the difference
                 sum = 0;
                 for (w = 0; w < N; w++) {
-                    if (kk[k][w] == 1) {
-                        //printf("win[%d][%d]: %lf wout[%d][%d]: %lf\n",k,w,win(k,w,urldata,urlgraph),k,w,wout(k,w,urldata,urlgraph) );
-                        //printf("sum: %lf\n", sum);
+                    if (kk[k][w] == 1) { //matching up inlinks
                         sum += urldata.data[w].pRank*(win(k,w,urldata,urlgraph))*(wout(k,w,urldata,urlgraph));
-                        //printf("sum: %lf\n", sum);
-                        if (k == 1) {
-                            //assert(1==2);
-                        }
                     }
                 }
 
-                for (w = 0; w < N; w++) {
-                    //printf("win[%d][%d]: %lf wout[%d][%d]: %lf\n",k,w,win(k,w,urldata,urlgraph),k,w,wout(k,w,urldata,urlgraph) );
-                        //sum += urldata.data[w].pRank*(win(k,w,urldata,urlgraph))*(wout(k,w,urldata,urlgraph));
-
-                }
-                // printf("sum: %.7f\n", sum);
-                // printf("prev prank: %.7f\n", prePR);
-                // printf("prank: %f\n", urldata.data[k].pRank);
-                 urldata.data[k].pRank = ((1 - d) / N) + (d * sum);
+                 urldata.data[k].pRank = ((1 - d) / N) + (d * sum); //as per page rank sum
                  diff += fabs(urldata.data[k].pRank-prePR);
-                // printf("diff:%lf\n", diff);
             }
-        //diff = 0;
         iteration++;
     }
     return;
@@ -169,14 +138,10 @@ double win(int x, int y, RankCollection urldata,Graph g){
     for (int b = 0; b < N; b++) {
         if (g->edges[y][b]) {
             temp = temp + urldata.data[b].inlinks;
-            //printf("temp: %f\n", temp);
         }
-        //printf("win temp: %f\n", temp);
     }
     float total = 0;
-    //printf("x inlinks: %f\n", urldata.data[x].inlinks);
     total = ((urldata.data[x].inlinks)/(temp));
-    //printf("%f\n", total);
     return total;
 }
 
@@ -190,9 +155,7 @@ double wout(int x, int y, RankCollection urldata, Graph g){
             }else{
                 temp = temp + urldata.data[b].outlinks;
             }
-            //printf("wout temp: %f\n", temp);
         }
-        //printf("%f\n", temp);
     }
     float total = 0;
     if (urldata.data[x].outlinks == 0) {
@@ -208,7 +171,7 @@ void exportPageRank(Graph g, RankCollection urldata,int N){
     src[0] = 'u';
     src[1] = 'r';
     src[2] = 'l';
-    src[3] = '\0'; //lmao what is wrong with me
+    src[3] = '\0'; //help
 
     for (int i = 0; i < g->nV; i++) {
         strcat(src, g->vertex[i]);
@@ -216,7 +179,7 @@ void exportPageRank(Graph g, RankCollection urldata,int N){
         src[3] = '\0';
     }
 
-    //sort these puppies
+    //sort urls according to their page rank (highest to lowest)
     sort(urldata, N);
 
     FILE *fp;
@@ -229,12 +192,9 @@ void exportPageRank(Graph g, RankCollection urldata,int N){
 }
 
 void sort(RankCollection urldata, int n) {
-    for(int i = 1; i < n; i++)
-    {
-        for(int j = 0; j < n-i; j++)
-        {
-            if(urldata.data[j].pRank < urldata.data[j+1].pRank)
-            {
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < n-i; j++){
+            if(urldata.data[j].pRank < urldata.data[j+1].pRank){ //bubble sort...sorry im lazy
                 Ranks temp = urldata.data[j];
                 urldata.data[j] = urldata.data[j+1];
                 urldata.data[j+1] = temp;
